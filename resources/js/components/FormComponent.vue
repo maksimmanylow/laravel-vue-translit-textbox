244517f1db63aef6ac0f672ef922816552486448
Maksim Manylow <maksimmanylow@gmail.com>
2018-10-18 17:36:30 +0300

    added transliterate and the opposite methods

--- resources/js/components/FormComponent.vue
+++ resources/js/components/FormComponent.vue
@@ -8,6 +8,8 @@
                     <div class="card-body">
                         <textarea v-model="value" />
                         <span>Символов введено: {{charCount}}</span>
+                        <button @click="transliterate">Транслитерировать</button>
+                        <button @click="untransliterate">Растранслитерировать</button>
                     </div>
                 </div>
             </div>
@@ -18,16 +20,31 @@
 <script>
 
     export default {
-        // methods: {
-        //     handleChange: function() {
-
-        //     }
-        // },
         data: () => ({
             value: '',
             }),
         computed: {
-            charCount: function () {return this.value.length;}
+            charCount: function () {return this.value.length;},
+        },
+        methods: {
+            transliterate: function(direction) {
+                let regexp = null;
+                let newText = text;
+                TranslitTable.forEach(char => {
+                    regexp = new RegExp(char.ru, 'g');
+                    newText = newText.replace(regexp, char.lat);
+                });
+                this.value = newText;
+            },
+        untransliterate: function () {
+                let regexp = null;
+                let newText = text;
+                TranslitTable.forEach(char => {
+                    regexp = new RegExp(char.lat, 'g');
+                    newText = newText.replace(regexp, char.ru);
+                });
+                this.value = newText;
+            }
         }
     }
 </script>
