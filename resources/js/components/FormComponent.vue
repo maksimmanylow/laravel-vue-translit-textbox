002b13b37f6e5786b0e7c1223cb3877858860b53
Maksim Manylow <maksimmanylow@gmail.com>
2018-10-18 15:43:00 +0300

    initial commit

---
+++ resources/js/components/FormComponent.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card card-default">
+                    <div class="card-header">Example Component</div>
+
+                    <div class="card-body">
+                        <textarea v-model="value" />
+                        <span>Символов введено: {{charCount}}</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        // methods: {
+        //     handleChange: function() {
+
+        //     }
+        // },
+        data: () => ({
+            value: '',
+            }),
+        computed: {
+            charCount: function () {return this.value.length;}
+        }
+    }
+</script>
